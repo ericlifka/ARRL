621aa264b0c9525e79d3646a5f5f7235fad7f056
Eric Lifka <eric.lifka@gmail.com>
2015-02-07 13:58:47 -0500

    simple environment implementation

--- ARRL.js
+++ ARRL.js
@@ -1,5 +1,25 @@
 window.ARRL = (function () {
-    return function rum() {
+    var Environment = (function () {
+        function Env() { }
+        Env.prototype.lookupSymbol = function (symbol) {
+            if (this.symbols.hasOwnProperty(symbol)) {
+                return this.symbols[symbol];
+            }
+            else if (this.parent) {
+                return this.parent.lookupSymbol(symbol);
+            }
+            else {
+                return null;
+            }
+        };
+        return function (parent) {
+            var e = new Env();
+            e.parent = parent;
+            e.symbols = {};
+        };
+    })();
+
+    return function rum(array) {
 
     };
 })();
