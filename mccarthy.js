a86407fe95f2f89afc0b277374991b5ddf9c44e3
Eric Lifka <eric.lifka@gmail.com>
2015-02-10 21:55:40 -0500

    top level conditions for eval

--- mccarthy.js
+++ mccarthy.js
@@ -1,3 +1,21 @@
 (function () {
 
+    function eval(expr, env) {
+        if (atom(expr)) {
+
+        }
+
+        else if (atom(car(expr))) {
+
+        }
+
+        else if (eq(caar(expr), 'label')) {
+
+        }
+
+        else if (eq(caar(expr), 'lambda')) {
+
+        }
+    }
+
 }());
